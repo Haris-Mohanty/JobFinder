ce2c7cd306b216459b245328cabaec397401866a
Haris Mohanty <harismohanty8658@gmail.com>
2023-05-26 12:08:14 +0530

    Package.json - Es module setup successfully.

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 //Express
-const express = require("express");
+// const express = require("express");
+import express from 'express';
 
 // Rest Object
 const app = express();
 
 // Rout Create
 app.get("/", (request, response) => {
-    response.send("<h1>Welcome to My JobFinder!</h1>");
+    response.send("<h1>Welcome to JobFinder!</h1>");
 });
 
 // Listen
 app.listen(8080, () => {
-    console.log("Node Server Running");
+    console.log("Node Server Running on Port 8080");
 });
