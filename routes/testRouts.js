7f0ca6aa9849286e9b62f3f2ab742c4a3dd46d6b
Haris Mohanty <harismohanty8658@gmail.com>
2023-05-27 21:17:48 +0530

    routes- testRoutes.js - import testPostController successfully.

--- routes/testRouts.js
+++ routes/testRouts.js
@@ -1,10 +1,11 @@
 import express from "express";
+import { testPostController } from "../controller/testController";
 
 //router object create
 const router = express.Router();
 
 // routes create
-router.post("/test",);
+router.post("/test",testPostController);
 
 //exports
 export default router;
