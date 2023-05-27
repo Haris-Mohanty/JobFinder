47797028ee7ee6614c9041c8465c05a8851a6541
Haris Mohanty <harismohanty8658@gmail.com>
2023-05-27 20:22:31 +0530

    db.js - connect with db code fixed.

--- config/db.js
+++ config/db.js
@@ -3,7 +3,9 @@ import colors from "colors";
 
 const connectDb = () => {
   try {
+    const conn = mongoose.connect(process.env.MONGO_URL);
+    console.log(`Connected to MongoDB ${mongoose.connection.host}`.bgMagenta.white);
   } catch (error) {
-    
+    console.log(`MongoDB Error ${error}`.bgRed.white);
   }
 };
