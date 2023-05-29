9776bcf4046e0c8967422a8f3c93a44d0032ded7
Haris Mohanty <harismohanty8658@gmail.com>
2023-05-29 10:55:52 +0530

    controller - authController.js - success code processing(Get all property like name, email, password form body successfully.).

--- controller/authController.js
+++ controller/authController.js
@@ -2,6 +2,7 @@ import e from "express";
 
 export const registerController = async (request, response) => {
   try {
+    const { name, email, password } = request.body;
   } catch (error) {
     console.log(error);
     response.status(400).send({
