2aa143b22b488b968e3d8668728fd3fd0f54c257
Haris Mohanty <harismohanty8658@gmail.com>
2023-05-29 10:56:42 +0530

    controller - authController.js - validate code processing...

--- controller/authController.js
+++ controller/authController.js
@@ -3,6 +3,9 @@ import e from "express";
 export const registerController = async (request, response) => {
   try {
     const { name, email, password } = request.body;
+    //VALIDATE
+    if (!name) {
+    }
   } catch (error) {
     console.log(error);
     response.status(400).send({
