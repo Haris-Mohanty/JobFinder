85f98a586a5aabc1d2ef9ccf7b07ebe1cc0d5499
Haris Mohanty <harismohanty8658@gmail.com>
2023-05-30 10:17:53 +0530

    middleware - errorMiddleware.js - Create a function errorMiddleware() succesfully.

--- middleware/errorMiddleware.js
+++ middleware/errorMiddleware.js
@@ -1,1 +1,7 @@
-//ERROR MIDDLEWARE
+//ERROR MIDDLEWARE CREATE || NEXT FUNCTION
+const errorMiddleware = (err, request, response, next) => {
+  console.log(err);
+  response.send(500).send({
+    su
+  });
+};
