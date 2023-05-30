c0d74b673b536e748dbab5b697d4e89dbfced24f
Haris Mohanty <harismohanty8658@gmail.com>
2023-05-30 11:10:10 +0530

    middleware - errorMiddleware.js - error message shorting successfully.

--- middleware/errorMiddleware.js
+++ middleware/errorMiddleware.js
@@ -1,15 +1,18 @@
 //ERROR MIDDLEWARE CREATE || NEXT FUNCTION
 const errorMiddleware = (err, request, response, next) => {
   console.log(err);
-  response.status(500).send({
-    success: false,
-    message: "Something Went Wrong",
-    err,
-  });
-
+  const defaultErrors = {
+    statusCode: 500,
+    message: err,
+  };
+  
+  
   //MISSING FIELD ERROR
   if (err.name == "ValidationError") {
+    defaultErrors.statusCode = 400;
+    defaultErrors.message = Object.values(err.errors).map((item) => item.message).join(",");
   }
+  response.status(defaultErrors.statusCode).json({message : defaultErrors.message});
 };
 
 export default errorMiddleware;
