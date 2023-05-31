8f7dd8d9097073a568c2007f59b3ab8abea7e74a
Haris Mohanty <harismohanty8658@gmail.com>
2023-05-31 10:58:50 +0530

    Model - userModel.js - create salt successfully and add 10 round.

--- models/userModel.js
+++ models/userModel.js
@@ -1,38 +1,40 @@
 import mongoose from "mongoose";
 import validator from "validator";
-import bcrypt from 'bcryptjs';
+import bcrypt from "bcryptjs";
 
 //Schema Create
 const userSchema = new mongoose.Schema({
-    name :{
-        type : String,
-        required : [true, 'Name is Required!']
-    },
-    lastName:{
-        type : String,
-    },
-    email:{
-        type : String,
-        required : [true, 'Email is Required!'],
-        unique : true,
-        validate : validator.isEmail
-    },
-    password:{
-        type : String,
-        required : [true, 'Password is Required!'],
-        minLength : [6, 'Password Length Should be Greater than 6 Charcter!']
-    },
-    location:{
-        type : String,
-        default:'India'
-    },
-    timeStamp :{
-        type: Date,
-        default: Date.now
-    }
+  name: {
+    type: String,
+    required: [true, "Name is Required!"],
+  },
+  lastName: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: [true, "Email is Required!"],
+    unique: true,
+    validate: validator.isEmail,
+  },
+  password: {
+    type: String,
+    required: [true, "Password is Required!"],
+    minLength: [6, "Password Length Should be Greater than 6 Charcter!"],
+  },
+  location: {
+    type: String,
+    default: "India",
+  },
+  timeStamp: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 //MIDDLEWARE CRATE
-userSchema
+userSchema.pre("save", async function () {
+  const salt = await bcrypt.genSalt(10);
+});
 
-export default mongoose.model('User', userSchema);
+export default mongoose.model("User", userSchema);
