9202f62943e22da5d8637eda929c76fd0694a862
Haris Mohanty <harismohanty8658@gmail.com>
2023-06-02 10:47:01 +0530

    middleware - authMiddleware.js - authorization failed message added.

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,7 +2,7 @@ import { Jwt } from "jsonwebtoken";
 
 const userAuth = async (request, response, next) => {
     const authHeader = request.headers.authorization;
-    if(!authHeader || authHeader.startsWith("Bearer")){
-        
+    if(!authHeader || !authHeader.startsWith("Bearer")){
+        next("Auth Failed!");
     }
 };
