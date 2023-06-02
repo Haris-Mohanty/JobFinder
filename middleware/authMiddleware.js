d8e953634139d06ed4e4bb0b13d16049e286d2b4
Haris Mohanty <harismohanty8658@gmail.com>
2023-06-02 10:46:25 +0530

    middleware - authMiddleware.js - check authheader and bearer

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,4 +2,7 @@ import { Jwt } from "jsonwebtoken";
 
 const userAuth = async (request, response, next) => {
     const authHeader = request.headers.authorization;
+    if(!authHeader || authHeader.startsWith("Bearer")){
+        
+    }
 };
