9f2215f5245ce4837e9a6afce361c667214f282a
Haris Mohanty <harismohanty8658@gmail.com>
2023-06-03 10:22:21 +0530

    routes - userRoutes.js - userAuth improrted from authMiddleware successfully.

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,13 @@
 import express from "express";
+import userAuth from "../middleware/authMiddleware";
 
 //ROUTER OBJECT CREATE
 const router = express.Router();
+
+// ROUTES CREATE
+// GET USERS || GET
+
+// UPDATE USER || PUT
+router.put("/update-user", userAuth);
 
 export default router;
