e5de6c5b99ad0fa730247b237e7363af34e482f3
Haris Mohanty <harismohanty8658@gmail.com>
2023-06-03 10:28:27 +0530

    controller - userController.js - validation processing...

--- controller/userController.js
+++ controller/userController.js
@@ -1,3 +1,5 @@
-export const updateUserController = () => {
-    
+export const updateUserController = async (request, response, next) => {
+  const { name, email, lastName, password } = request.body;
+    // validation
+    if()
 };
