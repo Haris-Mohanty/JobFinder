55cf454fdbb317797955cefc212f74e950d1c9f5
Haris Mohanty <harismohanty8658@gmail.com>
2023-06-03 10:32:11 +0530

    controller - userController.js - find query processing...

--- controller/userController.js
+++ controller/userController.js
@@ -1,10 +1,14 @@
-import userModel from "../models/userModel";
+import userModel from "../models/userModel.js";
 
 export const updateUserController = async (request, response, next) => {
-  const { name, email, lastName, password } = request.body;
+  const { name, email, lastName, location } = request.body;
   // validation
-  if (!name || !email || !lastName || !password) {
+  if (!name || !email || !lastName || !location) {
     next("Pleasr provide all fields!");
   }
-  const user = await userModel
+  const user = await userModel.findOne({ _id: request.user.userId });
+  user.name = name;
+  user.lastName = lastName;
+  user.email = email;
+  user.location = location;
 };
