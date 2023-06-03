14909be9ac4caff0b111c63fc5ad29db74209191
Haris Mohanty <harismohanty8658@gmail.com>
2023-06-03 10:29:54 +0530

    controller - userController.js - userModel imported successfully.

--- controller/userController.js
+++ controller/userController.js
@@ -1,5 +1,10 @@
+import userModel from "../models/userModel";
+
 export const updateUserController = async (request, response, next) => {
   const { name, email, lastName, password } = request.body;
-    // validation
-    if()
+  // validation
+  if (!name || !email || !lastName || !password) {
+    next("Pleasr provide all fields!");
+  }
+  const user = await userModel
 };
