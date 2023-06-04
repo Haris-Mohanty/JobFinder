d2ec23bbd68f95384872dd17d3fe922f121762aa
Haris Mohanty <harismohanty8658@gmail.com>
2023-06-04 20:42:50 +0530

    models - jobsmodel.js - jobSchema object creating...

--- models/jobsModel.js
+++ models/jobsModel.js
@@ -1,5 +1,33 @@
 import mongoose from "mongoose";
 
-const jobSchema = new mongoose.Schema({});
+const jobSchema = new mongoose.Schema({
+  company: {
+    type: String,
+    required: [true, "Company Name is Required!"],
+  },
+  position: {
+    type: String,
+    required: [true, "Job Position is Required!"],
+    minLength: 100,
+  },
+  status: {
+    type: String,
+    enum: ["pending", "reject", "interview"],
+    default: "pending",
+  },
+  workType: {
+    type: String,
+    enum: ["full-time", "part-time", "internship", "contract"],
+    default: "full-time",
+  },
+  workLocation: {
+    type: String,
+    default: "Mumbai",
+    required: [true, "Work Location is REquired!"],
+  },
+  createdBy: {
+    type : mongoose.Types
+  },
+});
 
 export default mongoose.model("Job", jobSchema);
