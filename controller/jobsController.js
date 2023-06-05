22e615612680bc7a9cdb4127b33d495f30bf168e
Haris Mohanty <harismohanty8658@gmail.com>
2023-06-05 19:06:41 +0530

    controller- jobsController.js- user checked(created by)

--- controller/jobsController.js
+++ controller/jobsController.js
@@ -1,6 +1,9 @@
 export const createJobController = async (request, response, next) => {
-  const {company, position} = request.body;
+  const { company, position } = request.body;
 
-  
-
+  //   VALIDATION
+  if (!company || !position) {
+    next("Please Provide All Fields!");
+  }
+  request.body.createdBy = request.userId;
 };
