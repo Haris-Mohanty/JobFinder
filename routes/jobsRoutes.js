d0165b87a3d4925decc32cfb98f9bc5f9e3bc405
Haris Mohanty <harismohanty8658@gmail.com>
2023-06-05 18:53:09 +0530

    routes- jobsroutes.js- add middleware userAuth.

--- routes/jobsRoutes.js
+++ routes/jobsRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
+import userAuth from "../middleware/authMiddleware";
 
 const router = express.Router();
 
 // ROUTES
 // CREATE JOBS || POST
-router.post('/create-job')
+router.post('/create-job', userAuth)
 
 
 export default router;
