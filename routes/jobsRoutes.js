e783675061c6a016e7d9cd9e111e768d2c284aff
Haris Mohanty <harismohanty8658@gmail.com>
2023-06-05 18:53:38 +0530

    routes- jobsroutes.js- routes created successfully.

--- routes/jobsRoutes.js
+++ routes/jobsRoutes.js
@@ -1,11 +1,10 @@
 import express from "express";
-import userAuth from "../middleware/authMiddleware";
+import userAuth from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 // ROUTES
 // CREATE JOBS || POST
-router.post('/create-job', userAuth)
-
+router.post("/create-job", userAuth);
 
 export default router;
