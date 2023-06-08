abdd06df90be56ffabc4b46c3e32d24af4c23a03
Haris Mohanty <harismohanty8658@gmail.com>
2023-06-08 12:29:09 +0530

    controller- jobsController.js- Add monthly application in response.

--- controller/jobsController.js
+++ controller/jobsController.js
@@ -128,6 +128,6 @@ export const jobStatsController = async (request, response) => {
   // Response Send
   response.status(200).json({
     totalJob: stats.length,
-    defaultStats,
+    monthlyApplication,
   });
 };
