f31e4cc4d1322fb18423a9a65d3a108002885473
Haris Mohanty <harismohanty8658@gmail.com>
2023-06-12 00:26:49 +0530

    routes- authRoutes.js - Add Ip limit code fixed.

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,7 +3,12 @@ import { loginController, registerController } from "../controller/authControlle
 import rateLimit from 'express-rate-limit';
 
 //IP LIMITER
-
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
 
 //ROUTER OBJECT
 const router = express.Router();
