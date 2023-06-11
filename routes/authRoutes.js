4c53599ca0bcd4a4fb7278f828e714af9f03dc76
Haris Mohanty <harismohanty8658@gmail.com>
2023-06-12 00:24:25 +0530

    routes- authRoutes.js - import rate limit successfully.

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { loginController, registerController } from "../controller/authController.js";
+import rateLimit from 'express-rate-limit';
 
 //ROUTER OBJECT
 const router = express.Router();
