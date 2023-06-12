a76979775c335a2a50c8f18925f19ef38e5110aa
Haris Mohanty <harismohanty8658@gmail.com>
2023-06-12 13:28:10 +0530

    routes -authRoutes.js - Swagger routes create...

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,7 +8,29 @@ const limiter = rateLimit({
 	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
 	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-})
+});
+
+//routes
+
+/**
+ * @swagger
+ * components:
+ *  schema:
+ *   User:
+ *    type:Object
+ *    required:
+ *     - name
+ *     - lastName
+ *     - email
+ *     - password
+ *     - location
+ *    properties:
+ *     id:
+ *      type: string
+ *      description: The Auto-generated id of user collection
+ */
+
+
 
 //ROUTER OBJECT
 const router = express.Router();
